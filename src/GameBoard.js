9779a73605bf6871be0d65f32df07264d9e1e588
priteshkulshreshtha <66051955+priteshkulshreshtha@users.noreply.github.com>
2021-01-31 17:39:50 +0530

    Update GameBoard.js

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -6,7 +6,7 @@ import WinStatus from './WinStatus';
 
 const GameBoard = (props) => {
     return (
-        <div>
+        <div className='hello'>
             <Board boardState={props.boardState} emptyIndex={props.boardState.indexOf(0)} onMove={props.onMove} />
             {!props.ai && <WinStatus boardState={props.boardState} emptyIndex={props.boardState.indexOf(0)} ai={props.ai} />}
             {props.ai && CheckWin(props.boardState) && <h1 className='win-message'>Solved it for you</h1>}
